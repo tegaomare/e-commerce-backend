0432cc8e591fd63ec33d2ccbbc9141262df31035
tegaomare <tegaomare@yahoo.com>
2023-06-26 18:52:13 -0400

    created the standard realationships

--- models/index.js
+++ models/index.js
@@ -5,13 +5,24 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Category, {
+  through: "ProductTag",
+  foreignKey: "category_id",
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: "ProductTag",
+  foreignKey: "category_id",
+});
 module.exports = {
   Product,
   Category,
