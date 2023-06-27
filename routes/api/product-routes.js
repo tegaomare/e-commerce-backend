364b815b7f8c4b80874abba1c825583215438dbd
tegaomare <tegaomare@yahoo.com>
2023-06-26 20:04:14 -0400

    updated

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // The `/api/products` endpoint
 
 // get all products
-router.get("/products", (req, res) => {
+router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
